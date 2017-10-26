8c703ce8d46a27cbc90e4b189ed18d70cc4a7b19
Aragon <aragonal@itc02v90bphtdg.local>
2017-10-26 13:37:26 -0700

    Adding example of Filter

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ import ProviderList from './ProviderList'
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = { providers: [] };
+		this.state = { providers: [], fullProvidersList:[] };
+		this.filterByName = this.filterByName.bind(this);
 	} 
 	 
 	componentDidMount() { 
 		fetch('//data.kingcounty.gov/resource/vkks-p86u.json?$order=kcbho_provider_network ASC')
 			.then(data => data.json())
-			.then(providers => this.setState({ providers }));
+			.then(providers => {
+				this.setState({ providers, fullProvidersList: providers });
+			});
+	}
+
+	filterByName(name){
+		const {fullProvidersList} = this.state;
+		const filteredProvidersList = fullProvidersList.filter(provider => {
+			return provider.kcbho_provider_network.toLowerCase().indexOf(name.toLowerCase()) > -1;
+		});
+		this.setState({providers:filteredProvidersList});
 	}
 	
 	render() {
-    const {providers} = this.state;
+		const {providers} = this.state;
 		return (
       <div className="row">
         <div className="col-sm-4">
-          <Filters />
+          <Filters filterByName={this.filterByName}/>
         </div>
         <div className="col-sm-8">
           <ProviderList providers={providers}  />
--- src/Filters.js
+++ src/Filters.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 class Filters extends React.Component {
+    
     render() {
         return <div className="call-out-text call-out-text-default">
             <h3>Filters can go here.</h3>
+            <button onClick={()=> this.props.filterByName("Annara")}>filter by Annara</button>
         </div>
     }
 }
